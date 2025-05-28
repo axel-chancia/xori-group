8996147844e876e7e6e72f7bbb34ee56d17a31ef
DOUMBA Arsel <amakita124@gmail.com>
2025-05-28 18:23:10 +0100

    Header 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
+
 import Heroe from '@/components/Heroe'
-=======
-import { HeroSection } from '@/components/HeroSection'
 import  Apropos from '@/components/Apropos'
 
->>>>>>> 4fb6bbb6a6e0ffaa1120afb31b631975d5b5af8b
 
 export default function HomePage() {
   return (
     <main className="flex flex-col space-y-32">
-<<<<<<< HEAD
       <Heroe />
-=======
-      <HeroSection />
       <Apropos />
-      
->>>>>>> 4fb6bbb6a6e0ffaa1120afb31b631975d5b5af8b
     </main>
 )
 }
