e2bbc0baa14664e2c863c575c3689300978287a3
DOUMBA Arsel <amakita124@gmail.com>
2025-05-29 00:47:15 +0100

    arsel

--- app/page.tsx
+++ app/page.tsx
@@ -1,105 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> cce40a7 (version 4)
-=======
->>>>>>> cb7f32a (version 3)
-=======
->>>>>>> ac27f30 (version 6)
-import SectionServices from "@/components/Section-service"; // Ensure the case matches
+import SectionServices from "@/components/Section-service";
 import Galeries from "@/components/Galeries";
->>>>>>> 9dcb365 (Ajout des sections Section-service et Galeries)
-=======
-=======
->>>>>>> e0d161a (Header 1)
-import SectionServices from "@/components/Section-service"; // Ensure the case matches
-import Galeries from "@/components/Galeries";
-=======
-import { HeroSection } from '@/components/HeroSection'
-=======
-
-import Heroe from '@/components/Heroe'
-<<<<<<< HEAD
->>>>>>> 8996147 (Header 1)
-import  Apropos from '@/components/Apropos'
->>>>>>> 4fb6bbb (Header 1)
-<<<<<<< HEAD
->>>>>>> b3bef06 (Header 1)
-=======
-=======
-import  Apropos from '@/components/About'
->>>>>>> 142a156 ( Changes to be committed:)
->>>>>>> 0ddebf9 ( Changes to be committed:)
-
-<<<<<<< HEAD
-import Heroe from '@/components/Heroe'
-import  Apropos from '@/components/Apropos'
-
+import Heroe from "@/components/Heroe";
+import Apropos from "@/components/About";
 
 export default function HomePage() {
-=======
-<<<<<<< HEAD
-export default function Home() {
->>>>>>> e0d161a (Header 1)
   return (
-<<<<<<< HEAD
     <main className="flex flex-col space-y-32">
       <Heroe />
       <Apropos />
+      <SectionServices />
+      <Galeries />
     </main>
-)
-=======
-    <div>
-  <SectionServices />
-  <Galeries/>
-    </div>
-      
   );
->>>>>>> 9dcb365 (Ajout des sections Section-service et Galeries)
 }
-
-=======
->>>>>>> d929c3e (version 4)
-=======
-import { HeroSection } from '@/components/HeroSection'
-=======
->>>>>>> 0a5f40b (version 6)
-import Heroe from '@/components/Heroe'
-=======
->>>>>>> 8996147 (Header 1)
-
-export default function HomePage() {
-  return (
-    <main className="flex flex-col space-y-32">
-      <Heroe />
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <HeroSection />
-
-=======
->>>>>>> ac27f30 (version 6)
-=======
-=======
-      <HeroSection />
-=======
->>>>>>> e0d161a (Header 1)
-      <Apropos />
-<<<<<<< HEAD
-      
->>>>>>> 4fb6bbb (Header 1)
-<<<<<<< HEAD
->>>>>>> b3bef06 (Header 1)
-=======
-=======
->>>>>>> 8996147 (Header 1)
->>>>>>> e0d161a (Header 1)
-    </main>
-)
-}
->>>>>>> 8f9a1c7 (version 3)
