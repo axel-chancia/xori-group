a9d09167c44ed7c7672211ca9996e843ad53af07
azuemedzo <steeazue@gmail.com>
2025-05-28 17:02:06 -0700

    Header 1

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 =======
 >>>>>>> cce40a7 (version 4)
@@ -12,6 +13,14 @@
 import SectionServices from "@/components/Section-service"; // Ensure the case matches
 import Galeries from "@/components/Galeries";
 >>>>>>> 9dcb365 (Ajout des sections Section-service et Galeries)
+=======
+import SectionServices from "@/components/Section-service"; // Ensure the case matches
+import Galeries from "@/components/Galeries";
+=======
+import { HeroSection } from '@/components/HeroSection'
+import  Apropos from '@/components/Apropos'
+>>>>>>> 4fb6bbb (Header 1)
+>>>>>>> b3bef06 (Header 1)
 
 import Heroe from '@/components/Heroe'
 import  Apropos from '@/components/Apropos'
@@ -46,12 +55,21 @@ import Heroe from '@/components/Heroe'
 export default function HomePage() {
   return (
     <main className="flex flex-col space-y-32">
+<<<<<<< HEAD
       <Heroe />
+<<<<<<< HEAD
 <<<<<<< HEAD
       <HeroSection />
 
 =======
 >>>>>>> ac27f30 (version 6)
+=======
+=======
+      <HeroSection />
+      <Apropos />
+      
+>>>>>>> 4fb6bbb (Header 1)
+>>>>>>> b3bef06 (Header 1)
     </main>
 )
 }
